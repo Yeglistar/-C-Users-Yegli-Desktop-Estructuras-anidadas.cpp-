5c15d66b8a772900b17f8cfc6b71d4712310b3d6
Yeglistar <126261604+Yeglistar@users.noreply.github.com>
2023-02-24 10:10:54 -0400

    Add files via upload

---
+++ Estructuras anidadas.cpp
@@ -0,0 +1,58 @@
+//PRÁCTICA 21/02
+//Yeglimar Montanez
+//Estructuras Anidadas
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct info_direccion{
+	char direccion[30];
+	char ciudad[20];
+	char estado[20];
+} ;
+
+struct empleado{
+	char nombre[20];
+	struct info_direccion dir_empleado;
+	double salario;
+}empleados[2];
+
+int main(){
+	for(int i=0;i<2;i++){
+		fflush(stdin);
+		printf("%i.Ingresa tu nombre:", i+1);
+		gets(empleados[i].nombre);
+		
+		printf("%i.Ingresa tu direccion:", i+1);
+		gets(empleados[i].dir_empleado.direccion);
+		
+		printf("%i.Ingresa tu ciudad:", i+1);
+		gets(empleados[i].dir_empleado.ciudad);
+		
+		printf("%i.Ingresa tu estado:", i+1);
+		gets(empleados[i].dir_empleado.estado);
+		
+		printf("%i.Ingresa tu salario:", i+1);
+		scanf("%lf", &empleados[i].salario);
+		
+		printf("\n");
+	}
+	for(int i=0; i<2; i++){
+		printf("\n\n Datos del empleado # %i", i+1);
+		printf("\nNombre %s", empleados[i].nombre);
+		printf("\nDireccion %s", empleados[i].dir_empleado.direccion);
+		printf("\nCiudad %s", empleados[i].dir_empleado.ciudad);
+		printf("\nEstado %s", empleados[i].dir_empleado.estado);
+		printf("\nSalario %.2lf", empleados[i].salario);
+	}
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
